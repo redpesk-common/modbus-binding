80d7d9f77b82eddc1bec9e7d13f7f483dfa65b10
José Bollo <jose.bollo@iot.bzh>
2024-01-30 11:18:10 +0100

    Remove superfluous files
    PKG_CONFIG_PATH is set by CMake, there is no need to set it here

--- conf.d/cmake/00-debian-config.cmake
+++
@@ -1,4 +0,0 @@
-message(STATUS "Custom options: 00-debian-config.cmake --")
-
-# LibModbus is not part of standard Linux Distro (https://libmodbus.org/)
-set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:/usr/local/lib64/pkgconfig")
--- conf.d/cmake/00-opensuse-config.cmake
+++
@@ -1,6 +0,0 @@
-message(STATUS "Custom options: 00-opensuse-config.cmake --")
-
-# LibModbus is not part of standard Linux Distro (https://libmodbus.org/)
-set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:/usr/local/lib64/pkgconfig")
-
-set(CMAKE_BUILD_TYPE "DEBUG")
